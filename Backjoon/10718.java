1a6004b169d2467f328a53e58c2afa5ffaf7d617
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-05-06 11:05:56 +0900

    Create 10718.java

---
+++ Backjoon/10718.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군\r\n" + 
+				                "강한친구 대한육군");
+	}
+}
+
+//\n, \r, \r\n 사용 가능하다
+//개행처리 문자 또는 escape 라고 일컫는다
