1a68cd04e303c32926814eec64e091132aa42bfc
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-05-09 17:14:06 +0900

    Create 10171.java

---
+++ Backjoon/10171.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( \')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
+
+// \n은 줄바꿈 즉, 개행을 하는 역할을 한다.
+// \\은 백슬래쉬를 출력할 때 사용한다.
+// \'은 '(작은따옴표) 를 출력할 때 사용한다.
