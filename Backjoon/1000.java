ed20d8c82a7a129b3592cb704c06d666478a479b
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-05-13 09:14:03 +0900

    Create 1000.java
    A + B

---
+++ Backjoon/1000.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		scanner.close();
+		System.out.println(A + B);
+		
+	}
+
+}
+
+/*scanner = 키보드로 입력받기 위한 방법의 클래스
+ * scanner import 필요
+ * 객체 생성
+ * 입력받기
+ */
+
+//문제 자체가 1 2 이렇게 공백으로 입력받는 것 주의한다.
