33eae2e35fae8c75b144aec6bd28d1c2a590a983
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-05-15 11:51:45 +0900

    Create 10869.java
    사칙연산

---
+++ Backjoon/10869.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class B2557 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		scanner.close();
+		System.out.println(A + B);
+		System.out.println(A - B);
+		System.out.println(A * B);
+		System.out.println(A / B);
+		System.out.println(A % B);
+		
+	}
+
+}
