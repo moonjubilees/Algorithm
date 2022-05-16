1554e325e836a44925e332fe6fc30e89045dcc4b
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-05-16 14:28:36 +0900

    Create 10926.java
    ??!

---
+++ Backjoon/10926.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String A = scanner.next();
+		
+		scanner.close();
+		System.out.println(A + "??!");
+	}
+
+}
