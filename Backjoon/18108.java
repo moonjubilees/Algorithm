436b02108a642fba2d1ad6457aeeeda84c5cbdcf
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-05-16 14:32:26 +0900

    Create 18108.java
    1998년생인 내가 태국에서는 2541년생?!

---
+++ Backjoon/18108.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		
+		scanner.close();
+		System.out.println(A - 543);
+	}
+
+}
+
