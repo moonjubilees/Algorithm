112ac2909264a7f7e309d202c53cc247ffc8c1fe
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-05-31 23:56:10 +0900

    Create 2742.java
    기찍 N

---
+++ Backjoon/반복문/2742.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) throws Exception {
+      Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        sc.close();
+        
+
+        for(int i =N; i>0; --i){
+        	System.out.println(i);
+        	
+        }
+    
+    }
+}
+
+
